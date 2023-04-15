a96271499454e736295ba6e67e304e6c61206df0
msdio <rlagofka1@gmail.com>
2023-04-16 00:33:06 +0900

    add router

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,35 @@
+import { Box, useMediaQuery } from "@mui/material";
+// import Header from "components/header";
+// import Landing from "components/landing";
+import { useRef } from "react";
+
+// import * as Guide from "../../components/guides";
+
+const Home = () => {
+  const observerRef = useRef<HTMLDivElement>(null);
+  const isMobile = useMediaQuery("(max-width: 900px)");
+
+  return (
+    <Box
+      width="100vw"
+      height="fit-content"
+      minHeight="100vh"
+      position="relative"
+    >
+      {/* <Header />
+      <Landing isMobile={isMobile} /> */}
+      <Box
+        ref={observerRef}
+        width="100%"
+        height="100vh"
+        zIndex="3"
+        bgcolor="#f9f9f9"
+      >
+        {/* <Guide.GuideUsage isMobile={isMobile} observerRef={observerRef} />
+        <Guide.GuideSample isMobile={isMobile} observerRef={observerRef} /> */}
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
---
+++ src/services/google-analytics/GoogleAnalyticsTracker.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import ReactGA from "react-ga4";
+import { useLocation } from "react-router-dom";
+
+const GoogleAnalyticsTracker = () => {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!window.location.href.includes("localhost")) {
+      ReactGA.initialize(
+        process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID as string
+      );
+    }
+    setInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.send({
+        hitType: "pageview",
+        page: location.pathname + location.hash,
+      });
+    }
+  }, [initialized, location]);
+};
+
+export default GoogleAnalyticsTracker;
